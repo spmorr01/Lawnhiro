1284cf8e37ddd3afdd705bdf138e6b7d500fabb3
Spencer Morris <spencermorris22@gmail.com>
2016-12-10 18:47:30 -0600

    Broken code, but working to parse response from Zillow.

--- app/src/main/java/com/willydevelopment/com/lawnhiro/HomeScreen.java
+++ app/src/main/java/com/willydevelopment/com/lawnhiro/HomeScreen.java
@@ -15,7 +15,22 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -33,6 +48,12 @@ public class HomeScreen extends AppCompatActivity {
     private String tempCity;
     private String tempState;
     private String tempZip;
+    private String tempFinalAddress;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client2;
 
 
     @Override
@@ -53,12 +74,15 @@ public class HomeScreen extends AppCompatActivity {
         zip = (EditText) findViewById(R.id.textDialogZip);*/
         //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         //fab.setOnClickListener(new View.OnClickListener() {
-            //@Override
-            //public void onClick(View view) {
-              //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
-            //}
+        //@Override
+        //public void onClick(View view) {
+        //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //        .setAction("Action", null).show();
+        //}
         //});
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -111,7 +135,7 @@ public class HomeScreen extends AppCompatActivity {
                 .setCancelable(false)
                 .setPositiveButton("OK",
                         new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) {
+                            public void onClick(DialogInterface dialog, int id) {
                                 // get user input and set it to result
                                 // edit text
                                 tempAddress1 = address1.getText().toString();
@@ -121,12 +145,12 @@ public class HomeScreen extends AppCompatActivity {
                                 tempZip = zip.getText().toString();
 
                                 finalAddress.setText(tempAddress1 + ", " + tempAddress2 + ", "
-                                + tempCity + ", " + tempState + ", " + tempZip);
+                                        + tempCity + ", " + tempState + ", " + tempZip);
                             }
                         })
                 .setNegativeButton("Cancel",
                         new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) {
+                            public void onClick(DialogInterface dialog, int id) {
                                 dialog.cancel();
                             }
                         });
@@ -139,32 +163,150 @@ public class HomeScreen extends AppCompatActivity {
 
     }
 
-    public void getPriceButtonClick (View view) {
-        String addressBodyText;
+    public void getPriceButtonClick(View view) {
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptsView = li.inflate(R.layout.price_prompt, null);
 
-            addressBodyText = finalAddress.getText().toString();
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
 
-            Email m = new Email("spencermorris22@gmail.com", "sp00k1obli182$");
-            String[] toArray = {"jj_morris10@hotmail.com"};
-            m.setTo(toArray);
-            m.setFrom("test@lawnhiro.com");
-            m.setSubject("This is an email sent using my Mail JavaMail wrapper from an Android device.");
-            m.setBody(addressBodyText);
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
 
-            try {
+        List lotSize = null;
+        NodeList nodes = null;
 
-                if(m.send()) {
-                    Toast.makeText(HomeScreen.this, "Email was sent successfully.", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(HomeScreen.this, "Email was not sent.", Toast.LENGTH_LONG).show();
-                }
-            } catch(Exception e) {
-                //Toast.makeText(MailApp.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show();
-                Log.e("HomeScreen", "Could not send email", e);
+        final EditText orderNotes = (EditText) promptsView
+                .findViewById(R.id.textNotes);
+
+        final TextView price = (TextView) promptsView
+                .findViewById(R.id.textPrice);
+
+        try {
+
+            URL url = new URL("http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz1fafu2i84y3_1hdll&address=2735%20S%2037th%20Street&citystatezip=Lincoln+NE+68506");
+            URLConnection conn = url.openConnection();
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(conn.getInputStream());
+
+            nodes = doc.getElementsByTagName("result");
+            //Log.println(1, "Nodes", nodes.toString());
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Element element = (Element) nodes.item(i);
+                NodeList title = element.getElementsByTagName("lotSizeSqFt");
+                Element line = (Element) title.item(0);
+                lotSize.add(line.getTextContent());
+                price.setText(lotSize.get(0).toString());
             }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
+        /*try {
+            URL url = new URL(
+                    "http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz1fafu2i84y3_1hdll&address=2735%20S%2037th%20Street&citystatezip=Lincoln+NE+68506");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new InputSource(url.openStream()));
+            doc.getDocumentElement().normalize();
+            NodeList nodeList = doc.getElementsByTagName("result");
+*//** Assign textview array lenght by arraylist size *//*
+            //name = new TextView[nodeList.getLength()];
+            //website = new TextView[nodeList.getLength()];
+            //category = new TextView[nodeList.getLength()];
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                //name[i] = new TextView(this);
+                //website[i] = new TextView(this);
+                //category[i] = new TextView(this);
+                Element lotSizeElement = (Element) node;
+                NodeList nameList = lotSizeElement.getElementsByTagName("lotSizeSqFt");
+                Element nameElement = (Element) nameList.item(0);
+                nameList = nameElement.getChildNodes();
+                name[i].setText("Name = "
+                        + ((Node) nameList.item(0)).getNodeValue());
+                NodeList websiteList = fstElmnt.getElementsByTagName("website");
+                Element websiteElement = (Element) websiteList.item(0);
+                websiteList = websiteElement.getChildNodes();
+                website[i].setText("Website = "
+                        + ((Node) websiteList.item(0)).getNodeValue());
+                category[i].setText("Website Category = "
+                        + websiteElement.getAttribute("category"));
+                layout.addView(name[i]);
+                layout.addView(website[i]);
+                layout.addView(category[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("XML Pasing Excpetion = " + e);
+        }
+*/
+
+
+        //price.setText(lotSize.get(0).toString());
+        //price.setText("$50");
+        //price.setText(nodes.getLength());
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("Accept",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // get user input and set it to result
+                                // edit text
+                                String addressBodyText;
+                                String priceText;
+                                String orderNotesText;
+
+                                addressBodyText = finalAddress.getText().toString();
+                                priceText = price.getText().toString();
+                                orderNotesText = orderNotes.getText().toString();
+
+                                Email m = new Email("order.lawnhiro@gmail.com", "0rd3rL@WN");
+                                String[] toArray = {"jj_morris10@hotmail.com"};
+                                m.setTo(toArray);
+                                m.setFrom("test@lawnhiro.com");
+                                m.setSubject("New Lawnhiro Order!");
+                                m.setBody("Address: " + addressBodyText + "\n"
+                                        + "Accepted Price: " + priceText + "\n"
+                                        + "Order Notes: " + orderNotesText);
+
+                                try {
+
+                                    if (m.send()) {
+                                        Toast.makeText(HomeScreen.this, "Email was sent successfully.", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(HomeScreen.this, "Email was not sent.", Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (Exception e) {
+                                    //Toast.makeText(MailApp.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show();
+                                    Log.e("HomeScreen", "Could not send email", e);
+                                }
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    public void acceptPriceButtonClick(View view) {
 
 
     }
+
+
 }
